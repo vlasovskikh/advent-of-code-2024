6ee4c1a88e2fd2c22a96657c5628426f03e64e33
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2024-12-01 21:37:57 +0300

    p01

--- aoc24/p01.py
+++ aoc24/p01.py
@@ -1,5 +1,38 @@
+from aoc24 import utils
+
+
+def total_distance(xs: list[int], ys: list[int]) -> int:
+    return sum(abs(x - y) for x, y in zip(sorted(xs), sorted(ys)))
+
+
+def similarity_score(xs: list[int], ys: list[int]) -> int:
+    cnt: dict[int, int] = {}
+    for y in ys:
+        if y in cnt:
+            cnt[y] += 1
+        else:
+            cnt[y] = 1
+    s = 0
+    for x in xs:
+        if x in cnt:
+            s += x * cnt[x]
+    return s
+
+
+def parse_input(lines: list[str]) -> tuple[list[int], list[int]]:
+    xs: list[int] = []
+    ys: list[int] = []
+    for line in lines:
+        x, y = line.split()
+        xs.append(int(x))
+        ys.append(int(y))
+    return xs, ys
+
+
 def main() -> None:
-    pass
+    xs, ys = parse_input(utils.read_input_lines(__file__))
+    print(total_distance(xs, ys))
+    print(similarity_score(xs, ys))
 
 
 if __name__ == "__main__":
--- tests/test_p01.py
+++ tests/test_p01.py
@@ -1,2 +1,27 @@
-def test_success() -> None:
-    assert 2 + 2 == 4
+from aoc24 import p01
+
+
+def test_example_1() -> None:
+    lines = """
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+""".strip().splitlines()
+    xs, ys = p01.parse_input(lines)
+    assert p01.total_distance(xs, ys) == 11
+
+
+def test_example_2() -> None:
+    lines = """
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+""".strip().splitlines()
+    xs, ys = p01.parse_input(lines)
+    assert p01.similarity_score(xs, ys) == 31
