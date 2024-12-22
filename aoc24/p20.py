754df4b1696866b7f4a2a2f52032e02d7a585997
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2024-12-22 19:56:06 +0300

    p20.2: Correct, but slow

--- aoc24/p20.py
+++ aoc24/p20.py
@@ -20,31 +20,38 @@ class Track(utils.Grid[Cell]):
         self.start = next(c for c in self if self[c] == Cell.START)
         self.end = next(c for c in self if self[c] == Cell.END)
 
-    def count_cheats(self) -> dict[int, int]:
+    def count_cheats(self, distance: int) -> dict[int, int]:
         cheats: set[Cheat] = set()
         for c in self:
             if self[c] != Cell.WALL:
-                cheats |= self.coord_cheats(c)
+                cheats |= self.coord_cheats(c, distance)
         paths = self.all_shortest_paths()
         savings = [s for cheat in cheats if (s := self.saving(cheat, paths)) > 0]
         return collections.Counter(savings)
 
-    def count_cheats_gte_100(self) -> int:
-        cheats = self.count_cheats()
+    def count_cheats_gte_100(self, distance: int) -> int:
+        cheats = self.count_cheats(distance)
         return sum(count for saving, count in cheats.items() if saving >= 100)
 
-    def coord_cheats(self, coord: utils.Coord) -> set[Cheat]:
-        cheats = [
-            utils.add_coord(coord, (0, 2)),
-            utils.add_coord(coord, (0, -2)),
-            utils.add_coord(coord, (2, 0)),
-            utils.add_coord(coord, (-2, 0)),
-        ]
-        return {(coord, c) for c in cheats if c in self and self[c] != Cell.WALL}
+    def coord_cheats(self, start: utils.Coord, distance: int) -> set[Cheat]:
+        """Find all cheat coordinates not farther than `distance`."""
+        sx, sy = start
+        cheats: set[Cheat] = set()
+        for x in range(sx - distance, sx + distance + 1):
+            for y in range(sy - distance, sy + distance + 1):
+                end = x, y
+                if (
+                    end != start
+                    and end in self
+                    and coord_distance(start, end) <= distance
+                    and self[end] != Cell.WALL
+                ):
+                    cheats.add((start, end))
+        return cheats
 
     def saving(self, cheat: Cheat, paths: dict[utils.Coord, int]) -> int:
         start, end = cheat
-        result = paths[end] - paths[start] - 2  # Cheat length
+        result = paths[end] - paths[start] - coord_distance(start, end)
         return result
 
     def all_shortest_paths(self) -> dict[utils.Coord, int]:
@@ -64,13 +71,19 @@ class Track(utils.Grid[Cell]):
         return best_score
 
 
+def coord_distance(c1: utils.Coord, c2: utils.Coord) -> int:
+    dx, dy = utils.sub_coord(c1, c2)
+    return abs(dx) + abs(dy)
+
+
 def parse_input(lines: list[str]) -> Track:
     return Track([[Cell(c) for c in line] for line in lines])
 
 
 def main() -> None:
     track = parse_input(utils.read_input_lines(__file__))
-    print(track.count_cheats_gte_100())
+    print(track.count_cheats_gte_100(2))
+    print(track.count_cheats_gte_100(20))
 
 
 if __name__ == "__main__":
--- tests/test_p20.py
+++ tests/test_p20.py
@@ -20,7 +20,7 @@ def test_example_1() -> None:
 ###############
 """.strip().splitlines()
     track = p20.parse_input(lines)
-    assert track.count_cheats() == {
+    assert track.count_cheats(2) == {
         2: 14,
         4: 14,
         6: 2,
@@ -32,4 +32,46 @@ def test_example_1() -> None:
         38: 1,
         40: 1,
         64: 1,
+    }
+
+
+def test_example_2() -> None:
+    lines = """
+###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############
+""".strip().splitlines()
+    track = p20.parse_input(lines)
+    cheats = {
+        saving: count
+        for saving, count in track.count_cheats(20).items()
+        if saving >= 50
+    }
+    assert cheats == {
+        50: 32,
+        52: 31,
+        54: 29,
+        56: 39,
+        58: 25,
+        60: 23,
+        62: 20,
+        64: 19,
+        66: 12,
+        68: 14,
+        70: 12,
+        72: 22,
+        74: 4,
+        76: 3,
     }
